969a25744995ddc1dfb945e3efe58fce417953d5
Parminder1998 <sp2913329@gmail.com>
2021-04-09 12:13:56 +1200

    Added base code

---
+++ ClassExample/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExample
+{
+    class Person
+    {
+        // Member Variables
+        // method s
+
+        private  string position { get;  set; }
+        private int salary { get; set; }
+        private string  department { get; set; }
+
+        // Methods
+
+        public void SetMember (string setdepartment, int setsalary, string department)
+
+        {
+            position = setposition;
+            salary = setsalary;
+            department =setdepartment
+        }
+
+        public void GetMember(out string getposition, out int getsalary, out string department)
+        {
+            getposition = position;
+            getsalary = salary;
+            getdepartment = department;
+
+        }
+
+
+    }
+
+    
+}
---
+++ ClassExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassExample
+{
+    class Program
+    {
+        public static Person obj = new Person();
+        public static Person obj1 = new Person();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Please enter position ");
+            string position = Console.ReadLine();
+            Console.WriteLine("Please enter salary ");
+            string salary = Console.ReadLine();
+            Console.WriteLine("please enter department");
+            string department = Console.ReadLine();
+
+            int newsalary = Convert.ToInt32(salary);
+
+            obj.SetMember(position, newsalary, department);
+
+            obj.GetMember(out position, out newsalary, out department);
+            Console.WriteLine("position: " + position + " salary " + newsalary);
+
+            Console.WriteLine("Please enter name for Obj1 ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter age Obj1");
+            age = Console.ReadLine();
+
+            newAge = Convert.ToInt32(age);
+
+            obj1.SetMember(name, newAge);
+            obj1.GetMember(out name, out newAge);
+            Console.WriteLine("Name: " + name + " Age " + newAge);
+        }
+    }
+}
